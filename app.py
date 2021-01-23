2265a76de4a1dcb99e7b15b121133a572fb7d0d4
foeuium <971334574@qq.com>
2021-01-23 19:27:03 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask,url_for,escape
+from flask import Flask,url_for,escape,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro!Try a test</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 #这种装饰器处理任意的name
 @app.route('/user/<name>')
 def user_page(name):
@@ -20,3 +20,18 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+#一些虚拟数据
+name = 'Zhang San'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
