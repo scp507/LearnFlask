1752bbf11cf4af2bc883897921a2bb0cdbb2388d
foeuium <971334574@qq.com>
2021-01-24 14:47:55 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ class Movie(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(60))
     year=db.Column(db.String(4))
+#多个模板都要用到user,因此用这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
 
 #数据库初始化函数，封装删除老数据库命令和建立新数据库命令
 @app.cli.command()  # 注册为命令
@@ -60,12 +65,12 @@ def forge():
 def index():
     user=User.query.first()
     movies=Movie.query.all()
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
 #这种装饰器处理任意的name
 @app.route('/user/<name>')
 def user_page(name):
     return '<h1>User page</h1>'
-
+#验证url_for的功能
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
@@ -77,6 +82,12 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+#自定义处理404错误
+@app.errorhandler(404)
+def page_not_found(e):
+    user=User.query.first()
+    return render_template('404.html'),404
 
 #一些虚拟数据
 # name = 'Zhang San'
